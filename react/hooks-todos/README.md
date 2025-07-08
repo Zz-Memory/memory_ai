f37a32711a151d6528b53fbd6c603457054ef904
Zz-Memory <Zz-Memory@outlook.com>
2025-07-08 21:40:45 +0800

    完善添加与删除todo-item

--- react/hooks-todos/README.md
+++ react/hooks-todos/README.md
@@ -28,3 +28,25 @@
   - 移动端的重要单位，不要用px（绝对单位，像素点）
     - 移动端 宽高不定 rem（html font-size） vw/vh（viewport） em（相对于自身的font-size，等比例） 相对单位
     - 使用相对单位，可以在所有设备上适配
+
+
+- 直接解构
+  const {
+    todos,
+    onAddTodo
+  } = props
+  - 变量 修改值
+    - 数据状态
+      - Data binding **数据绑定** jsx就是静态的
+        - {} 数据绑定
+      - 数据和界面状态的统一
+        - 界面由数据驱动
+
+
+- vue 和 react 区别
+  - 入门
+    - vue 好入门，api 好用
+    - react 倾向于原生JS，入门难
+      - hooks
+  - vue 双向绑定： <input v-model="text"/>
+  - react 单向绑定： <input value={text} onChange={() => setText(text)} />
--- react/hooks-todos/src/components/Todos/TodoForm.jsx
+++ react/hooks-todos/src/components/Todos/TodoForm.jsx
@@ -1,17 +1,39 @@
 // 私有状态
 import { useState } from "react";
 
-const TodoForm = () => {
-    // JSX 一定得有唯一的最外层元素 树状，一定有根节点，以树来编译解析JSX
-    return (
-        // <> 是 React.Fragment 的简写语法，也被称为短语法或空标签，是一个占位符，不会渲染到 DOM 中，同时又能将多个子元素组合在一起
-        <>
-            <h2 className="header">TodoForm</h2>
-            <form action="">
+const TodoForm = ({ onAddTodo }) => {
+  // 数据
+  // props 参数数据
+  // state 私有的数据
+  // 单向数据流
+  const [text, setText] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let result = text.trim(); // dry don't repeat yourself（不要重复自己）
+    if (!text.trim()) {
+      return;
+    }
+    onAddTodo(text.trim());
+    setText(""); // 数据状态和界面状态一致要敏感
+  };
 
-            </form>
-        </>
-    )
-}
+  // JSX 一定得有唯一的最外层元素 树状，一定有根节点，以树来编译解析JSX
+  return (
+    // <> 是 React.Fragment 的简写语法，也被称为短语法或空标签，是一个占位符，不会渲染到 DOM 中，同时又能将多个子元素组合在一起
+    <>
+      {/* <h2 className="header">TodoForm</h2> */}
+      <form action="" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={text}
+          onChange={e => setText(e.target.value)}
+          placeholder="Todo text"
+          required
+        />
+        <button type="submit">Add</button>
+      </form>
+    </>
+  );
+};
 
 export default TodoForm;

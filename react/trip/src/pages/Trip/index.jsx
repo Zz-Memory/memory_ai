7559121207da086c41943ed1753935a9145e10cc
Zz-Memory <Zz-Memory@outlook.com>
2025-07-25 17:09:45 +0800

    初步搭建ai聊天界面

--- react/trip/src/pages/Trip/index.jsx
+++ react/trip/src/pages/Trip/index.jsx
@@ -1,29 +1,45 @@
 import {
-    useEffect
+    useEffect,
+    useState,
 } from 'react';
 import useTitle from "@/hooks/useTitle";
 import {
     kimiChat,
 } from '@/llm';
-import styles from 'trip.module.css';
+import styles from '@/pages/Trip/trip.module.css';
+import {
+    Input,
+    Button,
+    Loading
+} from 'react-vant';
 
 const Trip = () => {
   useTitle("旅游聊天客服");
-  useEffect(() => {
-    const fetchChat = async() => {
-        const res = await kimiChat([
-            {
-                role: 'user',
-                content: '重庆旅游推荐'
-            }
-        ]);
-        console.log(res);
+  const [text, setText] = useState('');
+  const [isSending, setIsSending] = useState(false);
+
+  const handleChat = async () => {
+    if(text.trim() === '') {
+      return;
     }
-    fetchChat();
-  }, [])
+    setIsSending(true);
+  }
   return (
-    <div>
+    <div className='flex flex-col h-all'>
       <h1>Trip</h1>
+      <div className={`flex-1 ${styles.chatArea}`}>
+
+      </div>
+      <div className={`flex ${styles.inputArea}`}>
+        <Input
+            value={text}
+            onChange={(e) => {setText(e)}}
+            placeholder="请输入消息"
+            className={`flex-1 ${styles.input}`}
+        ></Input>
+        <Button disabled={isSending} type="primary" onClick={handleChat}>发送</Button>
+      </div>
+      {isSending && <div className="fixed-loading"><Loading type='ball' /></div>}
     </div>
   );
 };

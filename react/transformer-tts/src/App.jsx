28a19d2d884b59aa701e2261e7f8c3089f47c672
Zz-Memory <Zz-Memory@outlook.com>
2025-08-11 19:17:04 +0800

    初识端模型

--- react/transformer-tts/src/App.jsx
+++ react/transformer-tts/src/App.jsx
@@ -15,23 +15,22 @@ import {
 function App() {
   // 界面状态
   // llm ready 大模型准备好了不?
-  const [ready,setReady] = useState(null);
+  const [ready, setReady] = useState(null);
   // 按钮点击,防止多次点击
-  const [disabled,setDisabled] = useState(false);
+  const [disabled, setDisabled] = useState(false);
   // 进度条数组
-  const [progressItems,setProgressItems] = useState([]);
+  const [progressItems, setProgressItems] = useState([]);
   // 表单文本
-  const [text,setText] = useState('I love Hugging Face');
+  const [text, setText] = useState('I love Hugging Face');
   // 音色
-  const [selectedSpeaker,setSelectedSpeaker] = useState(DEFAULT_SPEAKER);
-  const [output,setOutput] = useState(null);
-
-    
+  const [selectedSpeaker, setSelectedSpeaker] = useState(DEFAULT_SPEAKER);
+  const [output, setOutput] = useState(null);
   const worker = useRef(null);
+
   useEffect(() => {
     // 引入transformer
     // http://localhost:5173/worker.js
-    worker.current = new Worker(new URL('./worker.js', import.meta.url),{
+    worker.current = new Worker(new URL('./worker.js', import.meta.url), {
       type: 'module'
     });
     worker.current.postMessage({
@@ -46,15 +45,79 @@ function App() {
       worker.current.removeEventListener('message', onMessageReceived);
     }
   }, [])
+  worker.current = new Worker(new URL('./worker.js', import.meta.url), {
+    type: 'module'
+  })
+  const handleGenerateSpeech = () => {
+    setDisabled(true);
+    worker.current.postMessage({
+      text,
+      speaker_id: selectedSpeaker
+    })
+  }
+
   return (
     <>
-      <div className="flex">
-        
-        {/* <Progress text='model' percentage={12}></Progress> */}
-        <AudioPlayer 
-          audioUrl="https://cdn.freesound.org/previews/819/819183_12880153-lq.mp3" 
-          mimeType="audio/mpeg"
-        />
+      <div className="min-h-screen flex justify-center items-center bg-gray-100">
+        <div className='bg-white p-8 rounded-lg w-full max-w-xl m-2'>
+          <h1 className="text-3xl font-semibold text-gray-800 mb-1 text-center">
+            In browser Text To Speech（端模型）
+          </h1>
+          <h2 className="text-base font-medium text-gray-700 mb-2 text-center">
+            Made with <a href="#" >Transformer.js</a>
+          </h2>
+          <div className="mb-4">
+            <label htmlFor="text" className="block text-sm font-medium text-gray-600">
+              Text
+            </label>
+            <textarea
+              id="text"
+              className='border border-gray-300 rounded-md p-2 w-full'
+              rows="4"
+              placeholder="Enter text here"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            ></textarea>
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="speaker"
+              className='block text-sm font-medium text-gray-600'
+            ></label>
+            <select
+              id="speaker"
+              className='border border-gray-300 rounded-md p-2 w-full'
+              value={selectedSpeaker}
+              onChange={(e) => setSelectedSpeaker(e.target.value)}
+            >
+              {
+                // 可迭代对象快速转换成数组[[key:value],[key1:value1],[key2:value2]]
+                Object.entries(SPEAKERS).map(([key, value]) => {
+                  return (
+                    <option key={key} value={value}>{key}:{value}</option>
+                  )
+                })
+              }
+            </select>
+          </div>
+          <div className="flex justify-center">
+            <button
+              className={`${disabled ?
+                'bg-gray-400 cursor-not-allowed' :
+                'bg-blue-500 hover-bg-blue-600'} rounded-md py-2 px-4`}
+              onClick={handleGenerateSpeech}
+              disabled={disabled}
+            >
+              {disabled ? 'Generating...' : 'Generate'}
+            </button>
+          </div>
+          {
+            output && <AudioPlayer
+              audioUrl={output}
+              mimeType={'audio/wav'}
+            />
+          }
+        </div>
       </div>
     </>
   )

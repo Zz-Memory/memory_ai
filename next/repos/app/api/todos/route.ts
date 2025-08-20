4d1ea0bdc35846ad33d898b0d52fd1af9aaef01a
Zz-Memory <Zz-Memory@outlook.com>
2025-08-20 19:16:36 +0800

    初识next.js

---
+++ next/repos/app/api/todos/route.ts
@@ -0,0 +1,45 @@
+import {
+    NextResponse // res
+} from 'next/server'; // api server
+
+// ts 是 js 的超集
+interface Todo {
+    id: number;
+    text: string;
+    completed: boolean
+}
+let todos: Todo[] = [
+    {
+        id: 1,
+        text: '去广州',
+        completed: false
+    },
+    {
+        id: 2,
+        text: '去深圳',
+        completed: false
+    }
+];
+
+// Restful 一切皆资源
+// 向用户暴露资源
+// method + 资源URL定义规则
+export async function GET() {
+    return NextResponse.json(todos);
+}
+
+export async function POST(request: Request) {
+    // 获取请求体
+    const body = await request.json();
+    // 核心的数据，函数的参数和返回值
+    const newTodo = {
+        id: + Date.now(),
+        text: body.text,
+        // typescript 除了强类型外，代码提示更好，写起来更快
+        completed: false
+    }
+    // 添加到todos
+    todos.push(newTodo);
+    // 返回响应
+    return NextResponse.json(newTodo); 
+}
